e1579efc993b6ee8afeb20a8303089a6593817a9
mahyadeh <62261680+mahyadeh@users.noreply.github.com>
2020-03-16 20:49:05 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,6 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(
-    # os.getenv('DB_USER', 'flask'),
-    # os.getenv('DB_PASSWORD', ''),
-    # os.getenv('DB_HOST', 'mysql'),
-    # os.getenv('DB_NAME', 'flask')
-    # )
-
-    # SECRET_KEY = environ.get('SECRET_KEY')
-    # FLASK_APP = environ.get('FLASK_APP')
-    # FLASK_ENV = environ.get('FLASK_ENV')
-    
-    #DB_USER = environ.get('DB_USER')
-    #DB_PASSWORD = environ.get('DB_PASSWORD')   
-    #DB_HOST = environ.get('DB_HOST')
-    #DB_NAME = environ.get('DB_NAME')    
-    
-    #SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}/{}'.format(
     os.getenv('DB_USER', 'flask'),
     os.getenv('DB_PASSWORD', ''),
